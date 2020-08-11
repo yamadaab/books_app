79d7ddedf765341f4176c651e65f33444c62babb
yamadaab <s.yamada.1125@gmail.com>
2020-08-11 19:39:00 +0900

    add picture column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_11_072355) do
+ActiveRecord::Schema.define(version: 2020_08_11_075220) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2020_08_11_072355) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "author"
+    t.string "picture"
   end
 
 end
