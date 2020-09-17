dab1065d7196fd9f7fd6cd310b05cc74dca05104
yamadaab <s.yamada.1125@gmail.com>
2020-09-17 19:50:07 +0900

    add omniauth

--- db/schema.rb
+++ db/schema.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -40,4 +38,5 @@ ActiveRecord::Schema.define(version: 2020_09_16_073433) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["uid", "provider"], name: "index_users_on_uid_and_provider", unique: true
   end
+
 end
