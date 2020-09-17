03d306f8da36308cbda7f635ac4feef4c63e8f99
yamadaab <s.yamada.1125@gmail.com>
2020-09-17 19:58:58 +0900

    rubocop

--- db/schema.rb
+++ db/schema.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -11,7 +13,6 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 2020_09_16_073433) do
-
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.text "memo"
@@ -39,5 +40,4 @@ ActiveRecord::Schema.define(version: 2020_09_16_073433) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["uid", "provider"], name: "index_users_on_uid_and_provider", unique: true
   end
-
 end
