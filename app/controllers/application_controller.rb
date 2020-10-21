a0e931368d8f334bf1b7af4f1da2713ccb29379c
yamadaab <s.yamada.1125@gmail.com>
2020-10-21 20:21:04 +0900

    add image upload function

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,6 +15,6 @@ class ApplicationController < ActionController::Base
 
  protected
    def configure_permitted_parameters
-     devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :postal_code, :adress, :introduction])
+     devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :postal_code, :adress, :introduction, :avatar])
    end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
+  has_one_attached :avatar
+  
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
          :omniauthable, omniauth_providers: %i[github]
