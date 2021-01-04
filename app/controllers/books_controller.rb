1abfce07f00c1427c0505b96a5c9d7ed0c27e0b8
yamadaab <s.yamada.1125@gmail.com>
2021-01-04 14:54:18 +0900

    delete jsons code

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -23,10 +23,8 @@ class BooksController < ApplicationController
     respond_to do |format|
       if @book.save
         format.html { redirect_to @book, notice: "Book was successfully created." }
-        format.json { render :show, status: :created, location: @book }
       else
         format.html { render :new }
-        format.json { render json: @book.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -35,10 +33,8 @@ class BooksController < ApplicationController
     respond_to do |format|
       if @book.update(book_params)
         format.html { redirect_to @book, notice: "Book was successfully updated." }
-        format.json { render :show, status: :ok, location: @book }
       else
         format.html { render :edit }
-        format.json { render json: @book.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -47,7 +43,6 @@ class BooksController < ApplicationController
     @book.destroy
     respond_to do |format|
       format.html { redirect_to books_url, notice: "Book was successfully destroyed." }
-      format.json { head :no_content }
     end
   end
 
