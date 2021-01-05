444d5ded9b4e582bfc5d4947cb8eb791c933fb12
yamadaab <s.yamada.1125@gmail.com>
2021-01-05 11:49:20 +0900

    delete respond_to

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -19,31 +19,24 @@ class ReportsController < ApplicationController
 
   def create
     @report = current_user.reports.build(report_params)
-
-    respond_to do |format|
-      if @report.save
-        format.html { redirect_to @report, notice: "Report was successfully created." }
-      else
-        format.html { render :new }
-      end
+    if @report.save
+      redirect_to @report, notice: "Report was successfully created."
+    else
+      render :new
     end
   end
 
   def update
-    respond_to do |format|
-      if @report.update(report_params)
-        format.html { redirect_to @report, notice: "Report was successfully updated." }
-      else
-        format.html { render :edit }
-      end
+    if @report.update(report_params)
+      redirect_to @report, notice: "Report was successfully updated." 
+    else
+      render :edit
     end
   end
 
   def destroy
     @report.destroy
-    respond_to do |format|
-      format.html { redirect_to reports_url, notice: "Report was successfully destroyed." }
-    end
+    redirect_to reports_url, notice: "Report was successfully destroyed."
   end
 
   private
